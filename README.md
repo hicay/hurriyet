345331c59e26a41dd2bc99c433636f48cce4dddc
Salih Çağlar İSPİRLİ <caglarispirli@gmail.com>
2016-09-24 23:40:16 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Hurriyet
+NodeJs Wrapper for Hurriyet API (soon)
+
+https://www.npmjs.com/package/hurriyet
+## Installation
+Install using npm
+```
+npm install hurriyet
+```
+
+## Usage
+Get an API key from [https://developers.hurriyet.com.tr](https://developers.hurriyet.com.tr)
+
+```
+var hurriyet = require('hurriyet');
+var h = new hurriyet('api token');
+```
+
+### Article
+##### Callback Function
+```javascript
+var newsCallback = function(err, data)
+  if (err)
+    console.log(err);
+  console.log(data);
+  //console.log(data[0].Description); // Description of first node
+};
+```
+##### Get daily news
+```javascript
+h.Articles.getAll(newsCallback); // Get daily news
+h.Articles.getAll({limit:4},newsCallback); // Get top 4 daily news
+h.Articles.getAll({limit:4,select:'Description'},newsCallback); // Get top 4 daily news' descriptions
+```
+##### Get single news from id
+```javascript
+h.Articles.getWithId(40199111,newsCallback); // Get news from id = 40199111
+h.Articles.getWithId(40199111,{select:'Description'},newsCallback); // Get news' description from id = 40199111
+```
---
+++ hurriyet.js
@@ -0,0 +1,79 @@
+// hurriyet.js
+var request = require('request');
+var endpoint = 'https://api.hurriyet.com.tr/v1/';
+var options = {
+  url: '',
+  headers: {
+    accept: 'application/json',
+    apikey: ''
+  }
+};
+
+// globefunc to api calls
+var callAPI = function (url,cb) {
+  options.url = endpoint+url;
+  function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      cb(error, JSON.parse(body));
+    }else{
+      cb(error, '');
+    }
+  };
+  request(options, callback);
+};
+
+var Hurriyet = function (apikey) {
+  options.headers.apikey = apikey;
+};
+
+
+Hurriyet.prototype = {
+  Articles : function Articles(){
+    console.log('articles');
+  },
+  Columns  : function Columns(){},
+  Date  : function Date(){/*sadece date dondurecek*/},
+  NewsPhotoGallery : function NewsPhotoGallery(){},
+  Page : function Page(){},
+  Path : function Path(){}
+}
+
+Hurriyet.prototype.Articles = {
+  ep : 'articles/',
+  getAll : function(opts, callback){
+    if(typeof callback === 'undefined' && typeof opts === 'function'){
+      return callAPI('articles', opts);
+    }
+    if (typeof callback === 'function') {
+      this.ep +='?'
+      if(opts.filter)
+        this.ep +='%24filter='+opts.filter;
+      if(opts.select)
+        this.ep +='&%24select='+opts.select;
+      if(opts.limit)
+        this.ep +='&%24top='+opts.limit;
+
+      console.log(this.ep);
+      return callAPI(this.ep, callback);
+    }
+
+  },
+  getWithId : function(id,opts,callback){
+    if(typeof opts === 'function'){
+      console.log('asd');
+      return callAPI(this.ep+id,opts);
+    }
+    if (typeof callback === 'function') {
+      this.ep += id+'?%24select='+opts.select;
+      return callAPI(this.ep, callback);
+    }
+  }
+};
+
+Hurriyet.prototype.info = function () {
+  console.log('Hurriyet !');
+  console.log('----');
+}
+
+
+module.exports = Hurriyet;
